bfc23fced65916f1d939c7b758ec539d1aa14c16
Rafian Sagar <sagarworld420@gmail.com>
2024-07-01 19:25:22 +0600

    Update db.php

--- db.php
+++ db.php
@@ -8,3 +8,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
+// CREATE TABLE posts (
+//     id INT(11) AUTO_INCREMENT PRIMARY KEY,
+//     title VARCHAR(255) NOT NULL,
+//     content TEXT NOT NULL,
+//     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+// );

1fc9192f1fe0602c26e10b65765d691e97da9db0
rafiansagar <sagarworld420@gmail.com>
2024-07-02 15:09:19 +0600

    update

--- api.php
+++ api.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "api_custom_data";
+include 'db.php';
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 $response = array();
--- insert.php
+++ insert.php
@@ -1,6 +1,11 @@
 <?php
 include 'db.php';
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $title = $_POST['title'];
 $content = $_POST['content'];
 
